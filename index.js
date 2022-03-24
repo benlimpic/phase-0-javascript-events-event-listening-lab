bd877a5d3fd2f217d62a1017d7c60470590b8393
init.defaultBranch <benlimpic@gmail.com>
2022-03-23 20:15:54 -0700

    commit tree 8a4bce35c4ffc6d4ced8d3778567201cac7a11e1

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
-function addingEventListener() {
+
+
+
+addingEventListener = () => {
+
+    const click = document.querySelector('#input')
+
+    const clickAlert = () => {
+        alert('dont click that')
+    }
+
+    click.addEventListener('click', clickAlert)
+
 }
+
+addingEventListener()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.getElementById('input');
+// input.addEventListener("click", function () {
+//     alert("I was clicked!");
+// })
+
+
+
+// const clickMe = document.querySelector('#input');
+// input.addEventListener('click', function () {
+//     alert("I was clicked!");
+// });
